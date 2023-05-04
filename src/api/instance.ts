086ea6b4deb20248807cfcea17b735da5b5fe5ca
seeunpark <senasoon0@gmail.com>
2023-05-04 14:46:16 +0900

    fix: api 호출시 console.info interceptors에서 작동하도록 수정

--- src/api/instance.ts
+++ src/api/instance.ts
@@ -2,4 +2,9 @@ import axios from 'axios';
 
 const api = axios.create({ headers: { 'Content-Type': 'application/json', 'Cache-Control': 'max-age=31536000' } });
 
+api.interceptors.request.use(config => {
+  console.info('calling api');
+  return config;
+});
+
 export default api;
--- src/api/search.ts
+++ src/api/search.ts
@@ -3,6 +3,5 @@ import { RecommendedSearchKeywords } from '@type/search';
 
 export const getRecommendedKeywords = async (keyword: string): Promise<RecommendedSearchKeywords[]> => {
   const response = await api.get(`api/v1/search-conditions/?name=${keyword}`);
-  console.info('calling api');
   return response.data;
 };
